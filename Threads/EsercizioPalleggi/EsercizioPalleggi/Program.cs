56c4c254bb1ab683b06421a9b8c90d2f7b5e2cb5
Jacopo Bianchi <jacopo.bianchi@issgreppi.it>
2024-12-17 21:16:17 +0100

    Esercizio SuperMarket

---
+++ Threads/EsercizioPalleggi/EsercizioPalleggi/Program.cs
@@ -0,0 +1,113 @@
+﻿
+internal class Program
+{
+    static SemaphoreSlim goGiovanni = new SemaphoreSlim(1, 1);
+    static SemaphoreSlim goMattia = new SemaphoreSlim(0, 1);
+    static SemaphoreSlim goAlessandro = new SemaphoreSlim(0, 1);
+    static SemaphoreSlim goRoberto = new SemaphoreSlim(0, 1);
+    static bool haFischiato = false;
+    private static readonly object _lockFischio = new object();
+
+    public static void Main(string[] args)
+    {
+        //simulo il fatto che dopo 10 secondi l'arbitro fischia
+        Task.Factory.StartNew(() => 
+        {
+            Task.Delay(1000).Wait();
+            lock(_lockFischio)
+            {
+                haFischiato = true;
+            }
+        });
+        Parallel.Invoke(() => GiovanniPlay(), () => MattiaPlay(), () => AlessandroPlay(), () => RobertoPlay());
+
+    }
+
+    private static void RobertoPlay()
+    {
+        bool fischiato = false;
+        lock (_lockFischio)
+        {
+            fischiato = haFischiato;
+        }
+        while (!fischiato)
+        {
+            fischiato = haFischiato;
+            //attendo il mio turno 
+            goRoberto.Wait();
+            //appena passo il wait ho passato il semaforo e sono in 
+            System.Console.WriteLine($"sono roberto e passo la palla a giovanni");
+            Random random = new Random();
+            int tempoCasuale = random.Next(100,301);
+            Thread.Sleep(tempoCasuale);
+            goGiovanni.Release();
+            System.Console.WriteLine($"ho tenuto la palla per {tempoCasuale} millisecondi");
+        }
+    }
+
+    private static void AlessandroPlay()
+    {
+        bool fischiato = false;
+        lock (_lockFischio)
+        {
+            fischiato = haFischiato;
+        }
+        while (!fischiato)
+        {
+            fischiato = haFischiato;
+            //attendo il mio turno 
+            goAlessandro.Wait();
+            //appena passo il wait ho passato il semaforo e sono in 
+            System.Console.WriteLine($"sono alessandro e passo la palla a roberto");
+            Random random = new Random();
+            int tempoCasuale = random.Next(100,301);
+            Thread.Sleep(tempoCasuale);
+            goRoberto.Release();
+            System.Console.WriteLine($"ho tenuto la palla per {tempoCasuale} millisecondi");
+        }
+    }
+
+    private static void MattiaPlay()
+    {
+        bool fischiato = false;
+        lock (_lockFischio)
+        {
+            fischiato = haFischiato;
+        }
+        while (!fischiato)
+        {
+            fischiato = haFischiato;
+            //attendo il mio turno 
+            goMattia.Wait();
+            //appena passo il wait ho passato il semaforo e sono in 
+            System.Console.WriteLine($"sono mattia e passo la palla a roberto");
+            Random random = new Random();
+            int tempoCasuale = random.Next(100,301);
+            Thread.Sleep(tempoCasuale);
+            goAlessandro.Release();
+            System.Console.WriteLine($"ho tenuto la palla per {tempoCasuale} millisecondi");
+        }
+    }
+
+    private static void GiovanniPlay()
+    {
+        bool fischiato = false;
+        lock (_lockFischio)
+        {
+            fischiato = haFischiato;
+        }
+        while (! fischiato)
+        {
+            fischiato = haFischiato;
+            //attendo il mio turno 
+            goGiovanni.Wait();
+            //appena passo il wait ho passato il semaforo e sono in 
+            System.Console.WriteLine($"sono giovanni e passo la palla a mattia");
+            Random random = new Random();
+            int tempoCasuale = random.Next(100,301);
+            Thread.Sleep(tempoCasuale);
+            goMattia.Release();
+            System.Console.WriteLine($"ho tenuto la palla per {tempoCasuale} millisecondi");
+        }
+    }
+}
---
+++ Threads/SuperMarket/SuperMarket/Program.cs
@@ -0,0 +1,46 @@
+﻿
+internal class Program
+{
+    static int incassoTotale = 0;
+    static readonly object _lock = new object();
+    static SemaphoreSlim semCliente = new SemaphoreSlim(1, 1);
+    static SemaphoreSlim semBancone = new SemaphoreSlim(0, 1);
+
+    static Random random = new Random();
+    static int costoProdotto;
+    private static void Main(string[] args)
+    {
+        Thread bancone = new Thread(() => BanconeWork());
+        Thread cliente = new Thread(() => CLIenteWork());
+        bancone.Start();
+        cliente.Start();
+    }
+
+    private static void CLIenteWork()
+    {
+        while (incassoTotale <100)
+        {
+            semCliente.Wait();
+            System.Console.WriteLine("sono il cliente e metto le cose sulla cassa");
+            Thread.Sleep(1000);
+            lock (_lock)
+            {
+                costoProdotto = random.Next(1, 101);
+                incassoTotale += costoProdotto;
+            }
+            System.Console.WriteLine($"ho pagato {costoProdotto} euro e me ne vado");
+            semBancone.Release();
+        }
+    }
+
+    private static void BanconeWork()
+    {
+        while (incassoTotale < 100)
+        {
+            semBancone.Wait();
+            System.Console.WriteLine($"sono il cassiere e ho prelevato {costoProdotto} euro dal cliente");
+            System.Console.WriteLine($"incasso attuale : {incassoTotale}");
+            semCliente.Release();
+        }
+    }
+}
